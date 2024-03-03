c9129677e694e322d3e7ee7d68202d565536d96a
06luki06 <lukas.riegler2000@gmail.com>
2024-03-03 20:55:14 +0100

    Update UI
    Demo data gets shown within a FlatList
    Add a placeholder for a button
    Add a placeholder for an input field
    Add a SafeAreaView for displaying the content under the notification bar.

--- react-native-tutorial/App.js
+++ react-native-tutorial/App.js
@@ -1,12 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Alert, FlatList } from 'react-native';
+// using for Android - SafeAreaView within 'react-native' only works for iOS
+import { SafeAreaView } from 'react-native-safe-area-context'; 
 
+
+const DATA = [
+  {
+    'id': '1',
+    title: 'Mediation'
+  },
+  {
+    'id': '2',
+    title: 'Coding'
+  },
+  {
+    'id': '3',
+    title: 'Journaling'
+  }
+]
+
+const TodoItem = (props) => (
+  <View style={styles.item}>
+    <Text style={styles.itemText}>{props.item.title}</Text>
+  </View>
+)
+
+// functional component
 export default function App() {
   return (
-    <View style={styles.container}>
+    // this matches with styles in line 15
+    // and container in line 16
+    // SafeAreaView is a wrapper for the view - to have the content go under the status bar
+    <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+      <TextInput style={styles.input}/>
+      <Button title='Start' onPress={() => Alert.alert('Simple Button pressed')}/>
+      <FlatList
+      style={styles.list}
+        data={DATA}
+        renderItem={({item}) => <TodoItem item={item}/>}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -17,4 +53,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    width: 200,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: 'gray',
+  },
+  list:{
+    alignSelf: 'stretch',
+  },
+  item:{
+    backgroundColor: '#6DB6DD',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 10,
+  },
+  itemText:{
+    color: '#FFFF'
+  }
 });
